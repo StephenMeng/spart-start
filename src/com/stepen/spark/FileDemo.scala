405d94024d5f6bfea1b3040077e6ff0b6f8494e8
stephenmeng <016611sai>
2018-04-25 19:22:37 +0800

    spark demo

--- src/com/stepen/spark/FileDemo.scala
+++ src/com/stepen/spark/FileDemo.scala
@@ -8,11 +8,12 @@ object FileDemo {
     //setMaster("local") 本机的spark就用local，远端的就写ip
     //如果是打成jar包运行则需要去掉 setMaster("local")因为在参数中会指定。
     conf.setMaster("local")
-    val sc =new SparkContext(conf)
-    val rdd =sc.textFile("C:\\Users\\DELL\\Desktop\\spark.txt")
-    var fileRDD=rdd.filter(_.contains("hello"))
-    fileRDD.cache()
-    fileRDD.count()
+    val sc = new SparkContext(conf)
+    partitionTest(sc)
   }
 
+  def partitionTest(sc: SparkContext): Unit = {
+    var rdd=sc.parallelize(1 to 10,2);
+    println(rdd.partitions.size)
+  }
 }
